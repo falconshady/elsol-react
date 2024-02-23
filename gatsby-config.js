390951670f8ff84b6af492c30df39ecbc27690eb
falconshady <falconshady@gmail.com>
2024-02-23 14:25:29 -0500

    remove dotenv call

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@
  * @type {import('gatsby').GatsbyConfig}
  */
 
-require("dotenv").config({
-    path: `.env.${process.env.NODE_ENV}`,
-})
+// require("dotenv").config({
+//     path: `.env.${process.env.NODE_ENV}`,
+// })
 
 module.exports = {
   siteMetadata: {
